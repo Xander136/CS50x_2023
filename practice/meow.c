4411624cd6e407a0c2886efdd1308ad0d00ad7dd
Xander136 <113091448+Xander136@users.noreply.github.com>
2023-02-20 14:26:45 +0000

    Mon, Feb 20, 2023, 2:26 PM +00:00

--- practice/meow.c
+++ practice/meow.c
@@ -1,13 +1,6 @@
 #include <stdio.h>
 
 int main(void) {
-    int counter = 0;
-
-    while (counter != 5)
-    {
-        printf("meow\n");
-        counter++;
-    }
-
-
+    for (int i = 0; i < 3; i++);
+    printf("meow\n");
 }

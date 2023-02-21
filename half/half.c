d7ba8287fcfdb04b3defdd313a20951a3b330e4e
Xander136 <113091448+Xander136@users.noreply.github.com>
2023-02-21 08:12:33 +0000

    Tue, Feb 21, 2023, 4:12 PM +08:00

--- half/half.c
+++ half/half.c
@@ -21,8 +21,10 @@ float half(float bill, float tax, int tip)
     //get bill + tax
     float tax_amount = (bill * (tax / 100));
     float bill_with_tax = tax_amount + bill;
-
     //get bill w/ tax + tip
+    float tip_converted = (float) tip / 100;
+    float tip_amount = (bill_with_tax * tip_converted);
     //divide by 2
+    float total_bill = (bill_with_tax + tip_amount)/2;
     return total_bill;
 }

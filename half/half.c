d5bd97ae792b8458bb2dcb39eeb5d9bc7df79726
Xander136 <113091448+Xander136@users.noreply.github.com>
2023-02-21 07:19:38 +0000

    Tue, Feb 21, 2023, 3:19 PM +08:00

--- half/half.c
+++ half/half.c
@@ -18,6 +18,8 @@ int main(void)
 // TODO: Complete the function
 float half(float bill, float tax, int tip)
 {
-    bill_amount = bill_amount * (tax_percent / 100);
+    float tax_inclusive_bill = bill * (tax / 100);
+    float plus_tip = tax_inclusive_bill * (tip/100);
+    float total = 
     return 0.0;
 }

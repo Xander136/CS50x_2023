9a33302eb2c1b16dbe20927029477fca022daf59
Xander136 <113091448+Xander136@users.noreply.github.com>
2023-02-22 13:14:02 +0000

    Wed, Feb 22, 2023, 9:14 PM +08:00

--- population/population.c
+++ population/population.c
@@ -4,14 +4,20 @@
 int main(void)
 {
     // TODO: Prompt for start size
-    int i;
+    int start_size;
     do
     {
-        int start_size = get_int("Start size: ");
+        start_size = get_int("Start size: ");
     }
     while (start_size < 9);
 
     // TODO: Prompt for end size
+    int end_size;
+    do
+    {
+        end_size = get_int("End size: ");
+    }
+    while (end_size <= start_size);
 
     // TODO: Calculate number of years until we reach threshold
 

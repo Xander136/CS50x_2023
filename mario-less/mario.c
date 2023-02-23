4d71880c44bc648884d79ec0562e36ea04744b13
Xander136 <113091448+Xander136@users.noreply.github.com>
2023-02-23 01:50:10 +0000

    Thu, Feb 23, 2023, 9:50 AM +08:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -9,26 +9,27 @@ int main(void)
         no_block = get_int("Height: "); //get height of blocks from user until he inputs 1-8
     }
     while (no_block <= 0 || no_block >= 9);
-}
 
-int block;
-int space;
-int n = 1;
 
-while (block < no_block) //loop until blocks is same as height
-{
-    space = (no_block - n);
-    block = (no_block - space);
-    while (0 > space) // loop until there are no more spaces
+    int block = 0;
+    int space = 0;
+    int n = 1;
+
+    while (block < no_block) //loop until blocks is same as height
     {
-        printf(" ");
-        space--;
+        space = (no_block - n);
+        block = (no_block - space);
+        while (0 > space) // loop until there are no more spaces
+        {
+            printf(" ");
+            space--;
+        }
+        while (0 > block) //loop until there are no more blocks
+        {
+            printf("#");
+            block--;
+        }
+        printf("\n"); //new line after each iteration
+        n++; //increase count
     }
-    while (0 > block) //loop until there are no more blocks
-    {
-        printf("#");
-        block--;
-    }
-    printf(\n); //new line after each iteration
-    n++; //increase count
 }

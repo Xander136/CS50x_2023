7b4e0ba2cca7ad3720a8213105da6daa4ef70759
Xander136 <113091448+Xander136@users.noreply.github.com>
2023-02-26 05:45:33 +0000

    Sun, Feb 26, 2023, 1:45 PM +08:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,7 +1,19 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
-    
+    do
+    {
+
+    }
+
+    //15-digit numbers & start with 34 or 37
+    printf("AMEX\n");
+    //16-digit numbers & start with 51, 52, 53, 54, or 55
+    printf("MASTERCARD\n");
+    //13- and 16-digit numbers & start with 4
+    printf("VISA\n");
+    //else
+    printf("INVALID\n");
 }

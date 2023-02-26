56bcf19b93c61a77f13ed7a58122adb9dc529482
Xander136 <113091448+Xander136@users.noreply.github.com>
2023-02-26 05:56:13 +0000

    Sun, Feb 26, 2023, 1:56 PM +08:00

--- credit/credit.c
+++ credit/credit.c
@@ -9,12 +9,27 @@ int main(void)
         card_no = get_long("Number: ");
     }
     while(card_no < 1);
+
+    string card_type = "";
+
     //15-digit numbers & start with 34 or 37
-    printf("AMEX\n");
+    if (card_type == "AMEX")
+    {
+        printf("AMEX\n");
+    }
     //16-digit numbers & start with 51, 52, 53, 54, or 55
-    printf("MASTERCARD\n");
+    if (card_type == "MASTERCARD")
+    {
+        printf("MASTERCARD\n");
+    }
     //13- and 16-digit numbers & start with 4
-    printf("VISA\n");
-    //else
-    printf("INVALID\n");
+    if (card_type == "VISA")
+    {
+        printf("VISA\n");
+    }
+    else
+    {
+        printf("INVALID\n");
+    }
+
 }

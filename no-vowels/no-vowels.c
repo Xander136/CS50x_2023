d7ddd28bb786479931c89abd58f106e52ed05b30
Xander136 <113091448+Xander136@users.noreply.github.com>
2023-03-02 05:18:28 +0000

    Thu, Mar 2, 2023, 1:18 PM +08:00

--- no-vowels/no-vowels.c
+++ no-vowels/no-vowels.c
@@ -23,8 +23,7 @@ int main(int argc, string argv[])
 }
 
 
-int main(void)
-{
+
     string s = get_string("Before: ");
     printf("After:  ");
     for (int i = 0, n = strlen(s); i < n; i++)
@@ -32,7 +31,7 @@ int main(void)
         printf("%c", toupper(s[i]));
     }
     printf("\n");
-}
+
 
 string replace(string word)
 {

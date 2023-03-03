c0acb0847c5733d5767882d92338a91acfe957e5
Xander136 <113091448+Xander136@users.noreply.github.com>
2023-03-03 05:56:38 +0000

    Fri, Mar 3, 2023, 1:56 PM +08:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,7 +1,27 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 
-int main(void) 
+int count_letters(string text);
+
+int main(void)
 {
+    string text = get_string("Text: ");
+    printf("%s\n", text);
+}
 
+//Function for counting letters from user input
+int count_letters(string text)
+{
+    int letters = 0;
+    //Loop over the string
+    for (i = 0, len = strlen(text); i < len; i++)
+    {
+        if (isalpha(text[i]))
+        {
+            letters++;
+        }
+    }
+    return letters;
 }

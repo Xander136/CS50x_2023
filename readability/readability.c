2a31e2527208a7279c5fde6768f2d56d61cf7c59
Xander136 <113091448+Xander136@users.noreply.github.com>
2023-03-03 06:06:44 +0000

    Fri, Mar 3, 2023, 2:06 PM +08:00

--- readability/readability.c
+++ readability/readability.c
@@ -4,16 +4,23 @@
 #include <ctype.h>
 
 int count_letters(string text);
+int count_words(string text);
 
 int main(void)
 {
     //get string of text
     string text = get_string("Text: ");
     // printf("%s\n", text);
+
     //count letters
     int letters = count_letters(text);
     //print number of letters
     printf("%i letters\n", letters);
+
+    //count words
+    int words = count_words(text);
+    //print number of words
+    printf("%i words\n", words);
 
 }
 
@@ -30,4 +37,19 @@ int count_letters(string text)
         }
     }
     return letters;
+}
+
+//Function for counting words from user input
+int count_words(string text)
+{
+    int words = 0;
+    //Loop over the string
+    for (int i = 0, len = strlen(text); i < len; i++)
+    {
+        if ((text[i]) == ' ')
+        {
+            words += 1;
+        }
+    }
+    return words;
 }

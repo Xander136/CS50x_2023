5836bac3b025008222d2c555d40fb88bbdb51a39
Xander136 <113091448+Xander136@users.noreply.github.com>
2023-03-03 07:01:23 +0000

    Fri, Mar 3, 2023, 3:01 PM +08:00

--- readability/readability.c
+++ readability/readability.c
@@ -30,7 +30,7 @@ int main(void)
 
     //Compute Coleman-Liau index
     int index = 0.0588 * (letters / words * 100) - 0.296 * (sentences / words * 100) - 15.8;
-    if (index < 1)
+    if (index < 0)
     {
         printf("Before Grade 1\n");
     }

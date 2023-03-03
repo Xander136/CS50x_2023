a02d45420ad5bc4e068cc68ee8cabe3c6c3efdea
Xander136 <113091448+Xander136@users.noreply.github.com>
2023-03-03 07:10:19 +0000

    Fri, Mar 3, 2023, 3:10 PM +08:00

--- readability/readability.c
+++ readability/readability.c
@@ -34,7 +34,7 @@ int main(void)
 
  */
     //Compute Coleman-Liau index
-    int index = 0.0588 * ((letters / words) * 100) - 0.296 * ((sentences / words) * 100) - 15.8;
+    int index = (0.0588 * ((letters / words) * 100)) - (0.296 * ((sentences / words) * 100)) - 15.8;
 
     if (index < 1)
     {

508be1aa8bd1710301fc883cf172ac6a80e1e6ab
Xander136 <113091448+Xander136@users.noreply.github.com>
2023-03-06 12:50:25 +0000

    Mon, Mar 6, 2023, 8:50 PM +08:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -1,18 +1,23 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
 
 
 int main(int argc, string argv[])
 {
     // Make sure program was run with just one command-line argument
-    do
-    {
-        int key = argv[3]
-    }
-    while (argc <)
-    // Make sure every character in argv[1] is a digit
 
+    if (argc != 2) //user must input KEY as command-line argument
+        printf("Usage: ./caesar key\n");
+    }
+
+    // Make sure every character in argv[1] is a digit
+        int key = argv[2];
+    for (i = 0, len = strlen(argv[2]); i < len; i++)
+    {
+        if ()
+    }
     // Convert argv[1] from a `string` to an `int`
 
     // Prompt user for plaintext

af263f8fdbbb6f81cb777294f49c2c4e73c16e9b
Xander136 <113091448+Xander136@users.noreply.github.com>
2023-03-06 13:46:32 +0000

    Mon, Mar 6, 2023, 9:46 PM +08:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -7,25 +7,17 @@
 int main(int argc, string argv[])
 {
     // Make sure program was run with just one command-line argument
-    if (argc == 2)
+    // Make sure every character in argv[2] is a digit
+    string key = argv[2];
+    for (int i = 0, len = strlen(argv[2]); i < len; i++)
     {
-        // Make sure every character in argv[2] is a digit
-        string key = argv[2];
-        for (int i = 0, len = strlen(argv[2]); i < len; i++)
+        char character = key[i];
+        if (isdigit(character) != 'True') //check if digit, else print usage
         {
-            char character = key[i];
-            if (isdigit(character) != 'True') //check if digit, else print usage
-            {
-                printf("Usage: ./caesar key\n");
-                break;
-            }
+            printf("Usage: ./caesar key\n");
+            break;
         }
     }
-    else
-    {
-        printf("Usage: ./caesar key\n");
-    }
-
     // Convert argv[1] from a `string` to an `int`
 
     // Prompt user for plaintext

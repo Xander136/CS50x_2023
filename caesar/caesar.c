776f758b5b44eb9d015bfb9d2909f1607cf58cf6
Xander136 <113091448+Xander136@users.noreply.github.com>
2023-03-07 01:05:43 +0000

    Tue, Mar 7, 2023, 9:05 AM +08:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
+#include <stdlib.h>
 
 bool only_digits(string s);
 
@@ -9,17 +10,28 @@ int main(int argc, string argv[])
 {
     // Make sure program was run with just one command-line argument
     // Make sure every character in argv[2] is a digit
-    if (argc != 2 || !(only_digits(argv[2])))
+    if (argc != 2 || !(only_digits(argv[1])))
     {
         printf("Usage: ./caesar key\n");
         return 1;
     }
 
     // Convert argv[1] from a `string` to an `int`
+    int key = atoi(argv[1]);
 
     // Prompt user for plaintext
+    string text = get_string("plaintext: ");
 
     // For each character in the plaintext:
+    printf("ciphertext: ");
+    for (i = 0, len = strlen(text); i < len; i++)
+    {
+        char letter = text[i];
+        if (isalpha(letter))
+        {
+            
+        }
+    }
 
         // Rotate the character if it's a letter
 }

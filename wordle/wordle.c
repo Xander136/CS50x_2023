e3df352658fd1de637b69fd98926bb3b770b945a
Xander136 <113091448+Xander136@users.noreply.github.com>
2023-03-09 13:25:03 +0000

    Thu, Mar 9, 2023, 9:25 PM +08:00

--- wordle/wordle.c
+++ wordle/wordle.c
@@ -35,11 +35,10 @@ int main(int argc, string argv[])
     }
 
     int wordsize = atoi(argv[1]);
-    int k = atoi(argv[1]);
 
     // ensure argv[1] is either 5, 6, 7, or 8 and store that value in wordsize instead
     // TODO #2
-    if (k < 5 || k > 8)
+    if (wordsize < 5 || wordsize > 8)
     {
         printf("Error: wordsize must be either 5, 6, 7, or 8\n");
         return 1;

414e63b183c0b9a89afb80f5f3059623cc66c794
Xander136 <113091448+Xander136@users.noreply.github.com>
2023-03-12 01:28:33 +0000

    Sun, Mar 12, 2023, 9:28 AM +08:00

--- max/max.c
+++ max/max.c
@@ -11,7 +11,7 @@ int main(void)
     do
     {
         n = get_int("Number of elements: ");
-    } 
+    }
     while (n < 1);
 
     int arr[n];
@@ -27,5 +27,9 @@ int main(void)
 // TODO: return the max value
 int max(int array[], int n)
 {
+    //declare max value
+    int max = array[0];
+    
+
     return 0;
 }

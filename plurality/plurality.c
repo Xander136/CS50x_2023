1f36e71ed8209a527b21c87dea02d1604698be93
Xander136 <113091448+Xander136@users.noreply.github.com>
2023-03-14 13:07:58 +0000

    Tue, Mar 14, 2023, 9:07 PM +08:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -87,9 +87,9 @@ void print_winner(void)
     // TODO
     int max_vote;
 
-    for (int i = 0; i < candidate_count; i++)
+    for (int i = 0, j = candidate_vote - 1; i < candidate_count; i++)
     {
-        
+        for
     }
 
     return;

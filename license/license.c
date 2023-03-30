526abe1bb05f3449fe115ef3aab0bcc50815e523
Xander136 <113091448+Xander136@users.noreply.github.com>
2023-03-30 03:37:17 +0000

    Thu, Mar 30, 2023, 11:37 AM +08:00

--- license/license.c
+++ license/license.c
@@ -31,7 +31,7 @@ int main(int argc, char *argv[])
         for (int i = 0; i < 7; i++)
         {
             // plates[idx][i] = malloc(strlen(buffer));
-            strcpy(*plates[idx][i], buffer);
+            strcpy(plates[idx][i], buffer);
         }
         idx++;
     }

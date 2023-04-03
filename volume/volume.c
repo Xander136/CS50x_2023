5fbaba079b5949c95dd020d6846107d0bc81f81a
Xander136 <113091448+Xander136@users.noreply.github.com>
2023-04-03 12:36:30 +0000

    Mon, Apr 3, 2023, 8:36 PM +08:00

--- volume/volume.c
+++ volume/volume.c
@@ -34,9 +34,11 @@ int main(int argc, char *argv[])
     float factor = atof(argv[3]);
 
     // TODO: Copy header from input file to output file
+    uint16_t header = //fread() //data from input file
 
     // TODO: Read samples from input file and write updated data to output file
-
+    //fwrite
+    
     // Close files
     fclose(input);
     fclose(output);

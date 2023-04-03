df05d7cadb685a86fe7ab860248733cbe7eb4e75
Xander136 <113091448+Xander136@users.noreply.github.com>
2023-04-03 12:21:00 +0000

    Mon, Apr 3, 2023, 8:21 PM +08:00

--- smiley/helpers.c
+++ smiley/helpers.c
@@ -7,9 +7,10 @@ void colorize(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
+            // change color
             if (image[i][j].rgbtRed == 0x00 && image[i][j].rgbtGreen == 0x00 && image[i][j].rgbtBlue == 0x00)
             {
-                image[i][j].rgbtRed = #FFFF00;
+                image[i][j].rgbtGreen = 0xFF;
             }
         }
     }

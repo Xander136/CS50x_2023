865eb42b36a5123f85e965c68800015b8d00625d
Xander136 <113091448+Xander136@users.noreply.github.com>
2023-04-03 13:56:14 +0000

    Mon, Apr 3, 2023, 9:56 PM +08:00

--- volume/volume.c
+++ volume/volume.c
@@ -36,7 +36,7 @@ int main(int argc, char *argv[])
     // TODO: Copy header from input file to output file
     uint8_t header[HEADER_SIZE];
     fread(header, HEADER_SIZE, 1, input);
-    fwrite(header, HEADER_SIZE, 1, input);
+    fwrite(header, HEADER_SIZE, 1, output);
 
     // TODO: Read samples from input file and write updated data to output file
     int16_t buffer;
@@ -44,7 +44,7 @@ int main(int argc, char *argv[])
     {
         //update volume
         buffer *= factor;
-        fwrite(&buffer, sizeof(int16_t), 1, input);
+        fwrite(&buffer, sizeof(int16_t), 1, output);
     }
 
 

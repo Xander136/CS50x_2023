a0f5cdbab7de5ad5b90655e2f75324dc72183d76
Xander136 <113091448+Xander136@users.noreply.github.com>
2023-04-05 15:20:52 +0000

    Wed, Apr 5, 2023, 11:20 PM +08:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -88,14 +88,16 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
         for (int j = 0; j < width; j++)
         {
             // calculate the average pixel value
-            int average = round((image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3.0);
 
-            // Set each color value to the average value
-            image[i][j].rgbtBlue = average;
-            image[i][j].rgbtGreen = average;
-            image[i][j].rgbtRed = average;
+            // int average = round((image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3.0);
+            image[i][j]
+            // // Set each color value to the average value
+            // image[i][j].rgbtBlue = average;
+            // image[i][j].rgbtGreen = average;
+            // image[i][j].rgbtRed = average;
         }
     }
+
     return;
 }
 

aba6a9866a67949ca64849d5b6483cc9366a1a46
Xander136 <113091448+Xander136@users.noreply.github.com>
2023-04-05 15:18:29 +0000

    Wed, Apr 5, 2023, 11:18 PM +08:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -80,7 +80,22 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
-    
+    // get each pixel
+    // height
+    for (int i = 0; i < height; i++)
+    {
+        // width
+        for (int j = 0; j < width; j++)
+        {
+            // calculate the average pixel value
+            int average = round((image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3.0);
+
+            // Set each color value to the average value
+            image[i][j].rgbtBlue = average;
+            image[i][j].rgbtGreen = average;
+            image[i][j].rgbtRed = average;
+        }
+    }
     return;
 }
 

197e55a05386839673fc042db74183d7d583631b
Xander136 <113091448+Xander136@users.noreply.github.com>
2023-04-06 04:26:43 +0000

    Thu, Apr 6, 2023, 12:26 PM +08:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -86,9 +86,11 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
     for (int i = 0; i < height; i++)
     {
         // width
-        for (int j = 0; j < half_width; j++)
+        for (int j = 0; j < half_width + 1; j++)
         {
-
+            RGBTRIPLE temp = image[i][j];
+            image[i][j] = image[i][width - j];
+            image[i][width - j]= temp;
         }
     }
     return;

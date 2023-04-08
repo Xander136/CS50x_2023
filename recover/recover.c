fb01c487da79dd78069b5a4e5e77a57416d087ba
Xander136 <113091448+Xander136@users.noreply.github.com>
2023-04-08 06:31:30 +0000

    Sat, Apr 8, 2023, 2:31 PM +08:00

--- recover/recover.c
+++ recover/recover.c
@@ -22,7 +22,7 @@ int main(int argc, char *argv[])
     }
 
     // read every byte until end of card
-    char buffer[512];
+    char buffer[513];
     fwrite(c, strlen(c) + 1, 1, fp);
         // read 512 bytes into a buffer
         // if start of new JPEG

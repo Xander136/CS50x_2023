6a1340c834cd295d22506f56bb01a562bf658fb5
Xander136 <113091448+Xander136@users.noreply.github.com>
2023-04-08 04:22:46 +0000

    Sat, Apr 8, 2023, 12:22 PM +08:00

--- recover/recover.c
+++ recover/recover.c
@@ -3,16 +3,23 @@
 
 int main(int argc, char *argv[])
 {
+    // check if there is exactly one command-line argument
     if (argc != 2)
     {
+        // If program is not executed with exactly one command-line argument, remind the user of correct usage
         printf("Usage: ./recover filename\n");
         return 1;
     }
 
-    FILE *file = fopen("arv[1]", "r");
+    // If the forensic image cannot be opened for reading,
+    // your program should inform the user as much, and main should return 1.
+    FILE *file = fopen(argv[1], "r");
     if (file == NULL)
     {
         printf("Can't open file: %s\n", argv[1]);
         return 1;
     }
+    // filename for generated image
+    char *filename;
+
 }

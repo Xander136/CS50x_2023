0af87416145dc8184f0e5d19f532f3f88a1a7906
Xander136 <113091448+Xander136@users.noreply.github.com>
2023-04-10 12:47:27 +0000

    Mon, Apr 10, 2023, 8:47 PM +08:00

--- reverse/reverse.c
+++ reverse/reverse.c
@@ -61,6 +61,7 @@ int main(int argc, char *argv[])
 
     // Write reversed audio to file
     // TODO #8
+    // read each block_size from the end
 
     // close input file
     if (inputPtr != NULL)

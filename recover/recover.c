a9cddc27f054bf8d64eac11f2cbea07421b78fbf
Xander136 <113091448+Xander136@users.noreply.github.com>
2023-04-10 02:26:42 +0000

    Mon, Apr 10, 2023, 10:26 AM +08:00

--- recover/recover.c
+++ recover/recover.c
@@ -64,7 +64,7 @@ int main(int argc, char *argv[])
 // check if start of JPEG
 int jpeg_header(BYTE buffer[])
 {
-    if ((buffer[0] == 0xff) && (buffer[1] == 0xd8) && (buffer[2] == 0xff) && (buffer[3] & 0xf0) == 0xe0)
+    if ((buffer[0] == 0xff) && (buffer[1] == 0xd8) && (buffer[2] == 0xff) && ((buffer[3] & 0xf0) == 0xe0))
     {
         return 0;
     }

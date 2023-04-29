f44826bd8e6676272442becceb8c923c4822669c
Xander136 <113091448+Xander136@users.noreply.github.com>
2023-04-29 12:11:18 +0000

    Sat, Apr 29, 2023, 8:11 PM +08:00

--- jar/jar.py
+++ jar/jar.py
@@ -1,6 +1,9 @@
 class Jar:
     def __init__(self, capacity=12):
-        ...
+        self.cookie_jar
+        if capacity < 0:
+            
+        self.capacity = capacity
 
     def __str__(self):
         ...

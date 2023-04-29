6517b2bf7540395a4ddd1ea41d506e4df404372b
Xander136 <113091448+Xander136@users.noreply.github.com>
2023-04-29 09:19:16 +0000

    Sat, Apr 29, 2023, 5:19 PM +08:00

---
+++ figlet/figlet.py
@@ -0,0 +1,8 @@
+## Expects zero or two command-line arguments:
+    ## Zero if the user would like to output text in a random font.
+    ## Two if the user would like to output text in a specific font
+        ## in which case the first of the two should be -f or --font
+        ## and the second of the two should be the name of the font.
+## Prompts the user for a str of text.
+
+## Outputs that text in the desired font.

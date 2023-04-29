e10f0973268ed4ee624d19c821c90333ed988623
Xander136 <113091448+Xander136@users.noreply.github.com>
2023-04-29 09:26:01 +0000

    Sat, Apr 29, 2023, 5:26 PM +08:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -1,6 +1,8 @@
-from pyfiglet import Figlet
+from pyfiglet import Figlet\
+import os
 
 ## Expects zero or two command-line arguments:
+
     ## Zero if the user would like to output text in a random font.
     ## Two if the user would like to output text in a specific font
         ## in which case the first of the two should be -f or --font
@@ -8,6 +10,6 @@ from pyfiglet import Figlet
 ## Prompts the user for a str of text.
 
 ## Outputs that text in the desired font.
-fonts = Figlet.getFonts(self)
+fonts = Figlet.getFonts()
 print(fonts)
 # figlet = Figlet()

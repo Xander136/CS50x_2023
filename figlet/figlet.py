fbfb6aa96e0927ccba708274ea5aa92dacd1cddd
Xander136 <113091448+Xander136@users.noreply.github.com>
2023-04-29 09:30:34 +0000

    Sat, Apr 29, 2023, 5:30 PM +08:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -1,12 +1,16 @@
 from pyfiglet import Figlet\
-import os
+from sys import argv
 
 ## Expects zero or two command-line arguments:
-
-    ## Zero if the user would like to output text in a random font.
+## Zero if the user would like to output text in a random font.
+if len(argv) == 0:
+    font = random()
+elif len(argv) == 2:
     ## Two if the user would like to output text in a specific font
         ## in which case the first of the two should be -f or --font
         ## and the second of the two should be the name of the font.
+else:
+    print("Usage: Expects zero or two command-line arguments ")
 ## Prompts the user for a str of text.
 
 ## Outputs that text in the desired font.

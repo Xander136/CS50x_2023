45cf522bdc50499057a646acb2dc600086af4f52
Xander136 <113091448+Xander136@users.noreply.github.com>
2023-04-29 08:37:48 +0000

    Sat, Apr 29, 2023, 4:37 PM +08:00

--- bank/bank.py
+++ bank/bank.py
@@ -1,16 +1,20 @@
-import sys
+
 
 # prompts the user for a greeting
 greeting = input("Greeting: ")
 greeting = greeting.islower()
 
 # If the greeting starts with “hello”,
-if greeting.startswith(prefix[hello, start[, end]])
-    # output $0.
+if greeting.startswith("hello"):
+    print("$0")
+
 # If the greeting starts with an “h” (but not “hello”),
-    #output $20.
+elif greeting.startswith("h"):
+    print("$20")
+
 # Otherwise,
-    # output $100.
+else:
+    print("$100")
 
 # str.strip([chars]) str.strip([chars])
 # str.startswith(prefix[, start[, end]]) Return True if string starts with the prefix, otherwise return False

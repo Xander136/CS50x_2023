30894199669c1f2980bc385d3467d49dea2dfb02
Xander136 <113091448+Xander136@users.noreply.github.com>
2023-04-29 08:24:13 +0000

    Sat, Apr 29, 2023, 4:24 PM +08:00

---
+++ bank/bank.py
@@ -0,0 +1,7 @@
+# prompts the user for a greeting
+# If the greeting starts with “hello”,
+    # output $0.
+# If the greeting starts with an “h” (but not “hello”),
+    #output $20.
+# Otherwise,
+    # output $100.

b345bbffd889fa6940d2ed785cdeded0d7d718fc
Xander136 <113091448+Xander136@users.noreply.github.com>
2023-05-09 12:40:07 +0000

    Tue, May 9, 2023, 8:40 PM +08:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -1,1 +1,20 @@
-# TODO
+# create mario less with python
+
+import cs50
+
+while True:
+    no_blocks = get_int("Height: ")
+    if no_blocks in range(1, 10):
+        break
+
+block = 0
+space = 0
+n = 1
+
+while True:
+    space = no_block - n
+    block = no_block - space
+
+    while space > 0:
+        print(" ", end="")
+        space

072ef2ca92b11f2e796652b99be8c14cd124c49e
Xander136 <113091448+Xander136@users.noreply.github.com>
2023-05-09 13:03:25 +0000

    Tue, May 9, 2023, 9:03 PM +08:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -1,7 +1,6 @@
 # create mario less with python
 
-from cs50 import get_int
-
+# get value of 1 - 8; reject any other input
 while True:
     try:
         no_blocks = int(input("Height: "))
@@ -10,24 +9,30 @@ while True:
     except ValueError:
         print("The input was not a valid integer.")
 
+# set variables
 block = 0
 space = 0
 n = 1
 
 while True:
+    # get number of blocks and spaces to print
     space = no_blocks - n
     block = no_blocks - space
 
+    # print space
     while space > 0:
         print(" ", end="")
         space -= 1
 
+    # print blocks
     while block > 0:
         print("#", end="")
         block -= 1
 
+    # print new line
     print("")
     n += 1
 
+    # loop until all blocks are printed
     if no_blocks < n:
         break

28207d57d75b308714a210fbde77f51486137670
Xander136 <113091448+Xander136@users.noreply.github.com>
2023-05-09 13:13:36 +0000

    Tue, May 9, 2023, 9:13 PM +08:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -1,25 +1,26 @@
 # create mario more with python
 
 # get value of 1 - 8; reject any other input
-def main():
 
-    while True:
-        try:
-            no_blocks = int(input("Height: "))
-            if no_blocks in range(1, 9):
-                break
-        except ValueError:
-            print("The input was not a valid integer.")
+while True:
+    try:
+        no_blocks = int(input("Height: "))
+        if no_blocks in range(1, 9):
+            break
+    except ValueError:
+        print("The input was not a valid integer.")
 
-    # set variables
-    block = 0
-    space = 0
-    n = 1
+# set variables
+block = 0
+space = 0
+n = 1
 
 while True:
     # get number of blocks and spaces to print
     space = no_blocks - n
+    gap = "  "
     block = no_blocks - space
+    left_block, right_block = block
 
     # print space
     while space > 0:
@@ -27,7 +28,14 @@ while True:
         space -= 1
 
     # print blocks
-    while block > 0:
+    while left_block > 0:
+        print("#", end="")
+        block -= 1
+
+    print(gap)
+
+    # print blocks
+    while right_block > 0:
         print("#", end="")
         block -= 1
 

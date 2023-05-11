a21578e8de8bbd162f5ad6345ef1bbcdd5add282
Xander136 <113091448+Xander136@users.noreply.github.com>
2023-05-11 13:00:32 +0000

    Thu, May 11, 2023, 9:00 PM +08:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -1,57 +1,37 @@
-# create cash in python
+# make cash in python
+
 from cs50 import get_float
 
+# get number of coins owed
+while True:
+    coins_owed = get_float("Change owed: ")
+    if coins_owed > 0:
+        break
 
+# convert to int
+coins_owed = round(coins_owed * 100)
 
+# set number of coins to 0
+coins = 0
 
-def get_cents():
-    # Prompt user for number of cents
-    while True:
-        cents = get_float(input("Change owed: "))
-        if cents > 0:
-            break
+# get number of coins
+while coins_owed > 0:
+    # get number of quarters
+    if coins_owed >= 25:
+      coins_owed -= 25
+      coins += 1
 
-def calculate_quarters(cents):
-    # divide cents by 25 to get number of quarters
-    quarters = (cents/25.0)
-    return quarters
+    elif coins_owed >= 10:
+        coins_owed -= 10
+        coins += 1
 
-def calculate_dimes(cents):
-    # divide cents by 10 to get number of quarters
-    dimes = (cents / 10.0)
-    return dimes
+    elif coins_owed >= 5:
+        coins_owed -= 5
+        coins += 1
 
-def calculate_nickels(cents):
-    # divide cents by 5 to get number of nickels
-    nickels = (cents / 5.0)
-    return nickels
+    else:
+        coins_owed -= 1
+        coins += 1
 
-
-def calculate_pennies(cents):
-    pennies = (cents / 1.0)
-    return pennies
-
-# Ask how many cents the customer is owed
-cents = get_cents()
-
-# Calculate the number of quarters to give the customer
-quarters = calculate_quarters(cents)
-cents = cents - quarters * 25
-
-# Calculate the number of dimes to give the customer
-dimes = calculate_dimes(cents)
-cents = cents - dimes * 10
-
-# Calculate the number of nickels to give the customer
-nickels = calculate_nickels(cents)
-cents = cents - nickels * 5
-
-# Calculate the number of pennies to give the customer
-pennies = calculate_pennies(cents)
-cents = cents - pennies * 1
-
-# Sum coins
-coins = quarters + dimes + nickels + pennies
-
-# Print total number of coins to give the customer
+# print number of coins
 print(f"{coins}")

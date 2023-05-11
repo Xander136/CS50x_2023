f48728187625f2bbb58ac439437968b5e2084594
Xander136 <113091448+Xander136@users.noreply.github.com>
2023-05-11 12:06:55 +0000

    Thu, May 11, 2023, 8:06 PM +08:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -1,1 +1,41 @@
-# TODO
+# create cash in python
+
+def main():
+    cents = get_cents()
+    # Ask how many cents the customer is owed
+    cents = get_cents()
+
+    # Calculate the number of quarters to give the customer
+    quarters = calculate_quarters(cents)
+    cents = cents - quarters * 25
+
+    # Calculate the number of dimes to give the customer
+    dimes = calculate_dimes(cents)
+    cents = cents - dimes * 10
+
+    # Calculate the number of nickels to give the customer
+    nickels = calculate_nickels(cents)
+    cents = cents - nickels * 5
+
+    # Calculate the number of pennies to give the customer
+    pennies = calculate_pennies(cents)
+    cents = cents - pennies * 1
+
+    # Sum coins
+    coins = quarters + dimes + nickels + pennies
+
+    # Print total number of coins to give the customer
+    printf("%i\n", coins)
+
+def get_cents():
+def calculate_quarters(self, cents):
+def 
+def
+def
+
+
+
+
+
+
+main()

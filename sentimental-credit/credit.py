01e9e192ccb11067c6eb41a0cb5381e3fb1c38fe
Xander136 <113091448+Xander136@users.noreply.github.com>
2023-05-12 13:42:58 +0000

    Fri, May 12, 2023, 9:42 PM +08:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -6,3 +6,7 @@ from cs50 import get_string
  # prompts the user for a credit card number
 
 card_no = get_int("Number: ")
+
+# American Express uses 15-digit numbers,
+# MasterCard uses 16-digit numbers,
+# Visa uses 13- and 16-digit

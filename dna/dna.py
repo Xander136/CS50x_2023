20d2a58782473fcbfe41fb77cfa1b5335d81c4fc
Xander136 <113091448+Xander136@users.noreply.github.com>
2023-05-13 13:02:04 +0000

    Sat, May 13, 2023, 9:02 PM +08:00

--- dna/dna.py
+++ dna/dna.py
@@ -5,7 +5,8 @@ import sys
 def main():
 
     # TODO: Check for command-line usage
-    
+    try:
+        filename = 
     # TODO: Read database file into a variable
 
     # TODO: Read DNA sequence file into a variable

72a4a912633fb7b336364d2225457e8fbdfd72b3
Xander136 <113091448+Xander136@users.noreply.github.com>
2023-05-14 03:58:24 +0000

    Sun, May 14, 2023, 11:58 AM +08:00

--- dna/dna.py
+++ dna/dna.py
@@ -34,11 +34,11 @@ def main():
 
     # TODO: Check database for matching profiles
     str_to_match = len(str)
-    matched_str = 0
     match_found = False
     for person in range(len(data)):
-        for pattern in range(len(str)):
-            if int(data[person][str[pattern]]) == sequence_dict[str[pattern]]:
+        matched_str = 0
+        for pattern in str:
+            if int(data[person][pattern]) == sequence_dict[pattern]:
                 matched_str += 1
 
         if matched_str == str_to_match:

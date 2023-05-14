435ad72c27fbe776d4c4867f1fbb1c4618bef89c
Xander136 <113091448+Xander136@users.noreply.github.com>
2023-05-14 01:42:12 +0000

    Sun, May 14, 2023, 9:42 AM +08:00

--- dna/dna.py
+++ dna/dna.py
@@ -24,7 +24,7 @@ def main():
 
     # TODO: Find longest match of each STR in DNA sequence
     str = list(data[0].keys())
-    # str = str.pop()
+    str.pop(0)
     print(str)
     # TODO: Check database for matching profiles
 

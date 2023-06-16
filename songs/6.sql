c43cee1267ce591c6cbe2aeeada83340015c1cde
Xander136 <113091448+Xander136@users.noreply.github.com>
2023-06-16 12:04:23 +0000

    Fri, Jun 16, 2023, 8:04 PM +08:00

--- songs/6.sql
+++ songs/6.sql
@@ -1,1 +1,1 @@
-SELECT name FROM songs WHERE artist_id = (SELECT artist_id FROM artists WHERE name = 'Post Malone');
+SELECT name FROM songs WHERE artist_id = (SELECT id FROM artists WHERE name = 'Post Malone');

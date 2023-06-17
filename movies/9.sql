6ac788dace99645ed450283b6a617971c21876f5
Xander136 <113091448+Xander136@users.noreply.github.com>
2023-06-17 12:37:32 +0000

    Sat, Jun 17, 2023, 8:37 PM +08:00

--- movies/9.sql
+++ movies/9.sql
@@ -4,3 +4,5 @@
 -- No need to worry about people who have no birth year listed, so long as those who do have a birth year are listed in order.
 -- If a person appeared in more than one movie in 2004, they should only appear in your results once.
 
+SELECT people.name FROM people
+WHERE 

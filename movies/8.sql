83b193e80473bf70898aaec1c2c4dd1300a77db1
Xander136 <113091448+Xander136@users.noreply.github.com>
2023-06-17 12:27:52 +0000

    Sat, Jun 17, 2023, 8:27 PM +08:00

--- movies/8.sql
+++ movies/8.sql
@@ -2,6 +2,6 @@
 -- Your query should output a table with a single column for the name of each person.
 -- You may assume that there is only one movie in the database with the title Toy Story.
 
-SELECT star FROM stars WHERE id IN
+SELECT people FROM stars WHERE id IN
 (SELECT person_id FROM stars
 WHERE movie_id = (SELECT id FROM movies WHERE title = 'Toy Story'))

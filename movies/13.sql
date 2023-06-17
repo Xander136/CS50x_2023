e03d1a97742db551564ced0b0268320603d0745e
Xander136 <113091448+Xander136@users.noreply.github.com>
2023-06-17 13:52:44 +0000

    Sat, Jun 17, 2023, 9:52 PM +08:00

--- movies/13.sql
+++ movies/13.sql
@@ -4,6 +4,5 @@
 -- Kevin Bacon himself should not be included in the resulting list.
 
 SELECT p.name FROM people p
-JOIN stars s ON s.movie_id = m.id
-JOIN people p ON p.id = s.person_id
-WHERE p.name IN ('Johnny Depp', 'Helena Bonham Carter')
+JOIN stars s ON s.person_id = p.id
+JOIN movies m ON m.id = s.movie_id

f287c8c6073f15f6333dc86b29eff4c78802637d
Xander136 <113091448+Xander136@users.noreply.github.com>
2023-06-17 13:42:06 +0000

    Sat, Jun 17, 2023, 9:42 PM +08:00

--- movies/12.sql
+++ movies/12.sql
@@ -6,12 +6,6 @@
 SELECT m.title FROM movies m
 JOIN stars s ON s.movie_id = m.id
 JOIN people p ON p.id = s.person_id
-WHERE p.name = 'Johnny Depp' AS j
-WHERE p.name = 'Helena Bonham Carter starred' AS h
-INNER JOIN j.title = h.title;
-
-
-SELECT m.title FROM movies m
-JOIN stars s ON s.movie_id = m.id
-JOIN people p ON p.id = s.person_id
-WHERE p.name = 'Helena Bonham Carter starred';
+WHERE p.name IN ('Johnny Depp', 'Helena Bonham Carter starred')
+GROUP BY m.title
+HAVING COUNT (DISTINCT p.name) = 2;

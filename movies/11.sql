e1c237fa96bbaf1fdf45104cdef9d6bd86c5e50f
Xander136 <113091448+Xander136@users.noreply.github.com>
2023-06-17 13:07:19 +0000

    Sat, Jun 17, 2023, 9:07 PM +08:00

--- movies/11.sql
+++ movies/11.sql
@@ -9,8 +9,7 @@ WHERE s.person_id =
 (SELECT p.id FROM people p
 WHERE p.name = 'Chadwick Boseman'))
 ORDER BY (SELECT r.rating FROM ratings r WHERE r.movie_id IN
-(SELECT s.movie_id FROM stars s
-WHERE s.person_id =
-(SELECT p.id FROM people p
-WHERE p.name = 'Chadwick Boseman')))
+
+
+)
 LIMIT 5;

a9c342c69130b3383a740e5542da0a0412ed4802
Xander136 <113091448+Xander136@users.noreply.github.com>
2023-06-17 13:21:06 +0000

    Sat, Jun 17, 2023, 9:21 PM +08:00

--- movies/12.sql
+++ movies/12.sql
@@ -7,5 +7,4 @@ SELECT m.title FROM movies m
 JOIN stars s ON s.movie_id = m.id
 JOIN people p ON p.id = s.person_id
 JOIN ratings r ON r.movie_id = m.id
-WHERE p.name = 'Johnny Depp' AND 'Helena Bonham Carter'
-;
+WHERE p.name = 'Johnny Depp';

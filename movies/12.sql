dbae4d3107d253c9c89399ed7da2c0f4e00d5618
Xander136 <113091448+Xander136@users.noreply.github.com>
2023-06-17 13:38:40 +0000

    Sat, Jun 17, 2023, 9:38 PM +08:00

--- movies/12.sql
+++ movies/12.sql
@@ -7,8 +7,11 @@ SELECT m.title FROM movies m
 JOIN stars s ON s.movie_id = m.id
 JOIN people p ON p.id = s.person_id
 WHERE p.name = 'Johnny Depp' AS j
+WHERE p.name = 'Helena Bonham Carter starred' AS h
+INNER JOIN j.title = h.title;
+
 
 SELECT m.title FROM movies m
 JOIN stars s ON s.movie_id = m.id
 JOIN people p ON p.id = s.person_id
-WHERE p.name = 'Helena Bonham Carter starred');
+WHERE p.name = 'Helena Bonham Carter starred';

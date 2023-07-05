f535e050de190fc328f2005b2ca54a782d8fbe78
Xander136 <113091448+Xander136@users.noreply.github.com>
2023-07-05 12:04:42 +0000

    Wed, Jul 5, 2023, 8:04 PM +08:00

--- birthdays/app.py
+++ birthdays/app.py
@@ -27,7 +27,15 @@ def index():
     if request.method == "POST":
 
         # TODO: Add the user's entry into the database
+        # Access form data
+        name = request.form.get("name")
+        month = request.form.get("month")
+        day = request.form.get("day")
 
+        # Insert data into database
+        db.execute("INSERT INTO birthdays (name, month, day) VALUES(?, ?, ?)", name, month, day)
+
+        # go back to homepage
         return redirect("/")
 
     else:

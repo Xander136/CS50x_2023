2818e4a192090a271bab77af75b848f0d866996c
Xander136 <113091448+Xander136@users.noreply.github.com>
2023-07-15 10:14:17 +0000

    Sat, Jul 15, 2023, 6:14 PM +08:00

--- finance/app.py
+++ finance/app.py
@@ -65,7 +65,8 @@ def buy():
 
     # SELECT how much cash the user currently has in users.
     amount = db.execute("SELECT cash FROM users WHERE id = ?", session["user_id"])
-    return render_template("homepage.html", cash=amount["cash"])
+    share_price = amount[0]["cash"] - 
+    return render_template("homepage.html", cash=amount[0]["cash"])
 
 
 @app.route("/history")

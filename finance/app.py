051cfc03d232683291730551133ad234d29247d8
Xander136 <113091448+Xander136@users.noreply.github.com>
2023-07-19 12:20:35 +0000

    Wed, Jul 19, 2023, 8:20 PM +08:00

--- finance/app.py
+++ finance/app.py
@@ -36,30 +36,30 @@ def after_request(response):
 @login_required
 def index():
     """Show portfolio of stocks"""
-    transactions = db.execute(
-        "SELECT symbol, SUM(shares) FROM transactions WHERE user_id = :id GROUP BY symbol HAVING share_qty > 0", id=session["user_id"]
-    )
-    # get balance
-    cash = db.execute("SELECT cash FROM users WHERE id = :user_id", user_id=session["user_id"])[0]["cash"]
+    # transactions = db.execute(
+    #     "SELECT symbol, SUM(shares) FROM transactions WHERE user_id = :id GROUP BY symbol HAVING share_qty > 0", id=session["user_id"]
+    # )
+    # # get balance
+    # cash = db.execute("SELECT cash FROM users WHERE id = :user_id", user_id=session["user_id"])[0]["cash"]
 
-    # total values
-    total_share_value = cash
-    total_portfolio_value = cash
+    # # total values
+    # total_share_value = cash
+    # total_portfolio_value = cash
 
-    for transaction in transactions:
-        # which stocks the user owns
-        symbol = transaction["symbol"]
-        
-        # the numbers of shares owned
-        shares = transaction["shares"]
+    # for transaction in transactions:
+    #     # which stocks the user owns
+    #     symbol = transaction["symbol"]
 
-        # the current price of each stock,
-        price = lookup("symbol")
+    #     # the numbers of shares owned
+    #     shares = transaction["shares"]
 
-        # and the total value of each holding (i.e., shares times price).
-        # Also display the user’s current cash balance along with a grand total (i.e., stocks’ total value plus cash).
+    #     # the current price of each stock,
+    #     price = lookup("symbol")
 
-    return render_template("index.html", stocks=stocks)
+    #     # and the total value of each holding (i.e., shares times price).
+    #     # Also display the user’s current cash balance along with a grand total (i.e., stocks’ total value plus cash).
+    return apology("todo")
+    # return render_template("index.html", stocks=stocks)
 
 
 @app.route("/buy", methods=["GET", "POST"])

a283a7725a660043fafc81ea0502a100a5860bd4
Xander136 <113091448+Xander136@users.noreply.github.com>
2023-07-20 13:55:16 +0000

    Thu, Jul 20, 2023, 9:55 PM +08:00

--- finance/app.py
+++ finance/app.py
@@ -279,4 +279,8 @@ def register():
 @login_required
 def sell():
     """Sell shares of stock"""
-    return render_template("/sell")
+    transactions = db.execute(
+        "SELECT symbol, SUM(share_qty) as total_shares FROM transactions WHERE user_id = :id GROUP BY symbol HAVING share_qty > 0", id=session["user_id"]
+    )
+
+    return render_template("/sell.html")

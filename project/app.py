4050096122c4ab9e782b2b5ee7ce49c270b69ec7
Xander136 <113091448+Xander136@users.noreply.github.com>
2023-07-23 14:26:05 +0000

    Sun, Jul 23, 2023, 10:26 PM +08:00

--- project/app.py
+++ project/app.py
@@ -57,8 +57,6 @@ def cards():
         english TEXT)
         """
         )
-    CREATE TABLE users (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, username TEXT NOT NULL, hash TEXT NOT NULL, cash NUMERIC NOT NULL DEFAULT 10000.00);
-CREATE UNIQUE INDEX username ON users (username);
 
     # render index template
     return render_template(

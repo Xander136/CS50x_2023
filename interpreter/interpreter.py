19d9e76937e049f3b10233fd35e45f0b69946b39
Xander136 <113091448+Xander136@users.noreply.github.com>
2023-08-13 14:02:32 +0000

    Sun, Aug 13, 2023, 10:02 PM +08:00

--- interpreter/interpreter.py
+++ interpreter/interpreter.py
@@ -1,1 +1,21 @@
 # get user input
+expression = input("Expression: ")
+expression = expression.split(" ")
+x = int(expression[0])
+y = expression[1]
+z = int(expression[2])
+
+
+match y:
+    case "+":
+        print(float(x + y))
+    case "-":
+        print(float(x - y))
+    case "*":
+        print(float(x * y))
+    case "/":
+        print(float(x / y))
+    case _:
+        print("Try again.")
+
+

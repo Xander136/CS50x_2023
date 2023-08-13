c7b1856d2a9d22325def216cc60e31a6e4142beb
Xander136 <113091448+Xander136@users.noreply.github.com>
2023-08-13 10:03:39 +0000

    Sun, Aug 13, 2023, 6:03 PM +08:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -11,8 +11,16 @@ application/zip
 """
 
 img_extensions = [".gif", ".jpg", ".jpeg", ".png"]
+app_extensions = [".pdf", ".pdf"]
 # get user input
 file = input("File name: ").lower().strip()
 if any(file.endswith(extension) for extension in img_extensions):
     file_ext = file.split(".")[-1]
     print(f"image/{file}")
+
+elif any(file.endswith(extension) for extension in app_extensions):
+    file_ext = file.split(".")[-1]
+    print(f"application/{file}")
+
+elif file.endswith(".txt"):
+    print("text/plain")

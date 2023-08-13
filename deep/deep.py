fef48bf7e9e5ef76ecc24bcd22c9b1e3016ed13a
Xander136 <113091448+Xander136@users.noreply.github.com>
2023-08-13 09:32:24 +0000

    Sun, Aug 13, 2023, 5:32 PM +08:00

--- deep/deep.py
+++ deep/deep.py
@@ -1,5 +1,5 @@
 # get user input
-answer = input("What is the Answer to the Great Question of Life, the Universe, and Everything?").lower()
+answer = input("What is the Answer to the Great Question of Life, the Universe, and Everything?").lower().strip()
 
 # evaluate answer
 if answer == "42" or answer == "forty-two" or answer == "forty two":

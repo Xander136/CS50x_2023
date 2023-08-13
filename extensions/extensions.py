085c6eb83b1c03977da6ada41b98c16da6efce2f
Xander136 <113091448+Xander136@users.noreply.github.com>
2023-08-13 09:48:17 +0000

    Sun, Aug 13, 2023, 5:48 PM +08:00

---
+++ extensions/extensions.py
@@ -0,0 +1,15 @@
+"""
+media types
+
+image/gif
+image/jpg
+image/jpeg
+image/png
+application/pdf
+text/plain
+application/zip
+"""
+
+# get user input
+file = input("File name: ").lower().strip()
+if file.endswith(".gif"):

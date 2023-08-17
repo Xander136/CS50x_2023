a0d81df5dddba822caf96bc242543c2b96e521ec
Xander136 <113091448+Xander136@users.noreply.github.com>
2023-08-17 13:49:10 +0000

    Thu, Aug 17, 2023, 9:49 PM +08:00

--- coke/coke.py
+++ coke/coke.py
@@ -5,12 +5,14 @@ COINS = ["25", "10", "5"]
 def get_change():
     change = ""
     while change not in COINS:
+        print(f"Amount Due: {amount_due}")
         change = input("Insert Coin: ")
     return int(change)
 
 def main():
     amount_due = 50
     coins = 0
+
     while amount_due > 0:
         print(f"Amount Due: {amount_due}")
         change = int(get_change())

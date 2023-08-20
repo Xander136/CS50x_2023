4620734fbc5afe988f476d65991da6b25c12d4f6
Xander136 <113091448+Xander136@users.noreply.github.com>
2023-08-20 06:32:07 +0000

    Sun, Aug 20, 2023, 2:32 PM +08:00

---
+++ nutrition/nutrition.py
@@ -0,0 +1,30 @@
+def main():
+    # get user input
+    item = input("Item: ")
+
+    fruits = [
+        {"name": "apple", "calories": 130},
+        {"name": "avocado california", "calories": 50},
+        {"name": "banana", "calories": 110},
+        {"name": "cantaloupe", "calories": 50},
+        {"name": "grapefruit", "calories": 60},
+        {"name": "grapes", "calories": 90},
+        {"name": "honeydew melon", "calories": 50},
+        {"name": "kiwifruit", "calories": 90},
+        {"name": "Lemon", "calories": },
+        {"name": "", "calories": },
+        {"name": "", "calories": },
+        {"name": "", "calories": },
+        {"name": "", "calories": },
+        {"name": "", "calories": },
+        {"name": "", "calories": },
+        {"name": "", "calories": },
+        {"name": "", "calories": },
+    ]
+
+    # print result
+    calories = ""
+    print(calories)
+
+
+main()

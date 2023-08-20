68674bb39db7b2b7262294209c7ac15a732b1d1e
Xander136 <113091448+Xander136@users.noreply.github.com>
2023-08-20 04:01:55 +0000

    Sun, Aug 20, 2023, 12:01 PM +08:00

---
+++ plates/plates.py
@@ -0,0 +1,14 @@
+def main():
+    # get plate
+    plate = input("Plate: ")
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
+
+
+def is_valid(s):
+    if not
+
+
+main()

587dd9ca8d878d1919b0aef8a2c0a3c229c5a75d
Xander136 <113091448+Xander136@users.noreply.github.com>
2023-08-20 06:35:55 +0000

    Sun, Aug 20, 2023, 2:35 PM +08:00

--- nutrition/nutrition.py
+++ nutrition/nutrition.py
@@ -11,19 +11,25 @@ def main():
         {"name": "grapes", "calories": 90},
         {"name": "honeydew melon", "calories": 50},
         {"name": "kiwifruit", "calories": 90},
-        {"name": "Lemon", "calories": },
-        {"name": "", "calories": },
-        {"name": "", "calories": },
-        {"name": "", "calories": },
-        {"name": "", "calories": },
-        {"name": "", "calories": },
-        {"name": "", "calories": },
-        {"name": "", "calories": },
-        {"name": "", "calories": },
+        {"name": "lemon", "calories": 15},
+        {"name": "lime", "calories": 20},
+        {"name": "nectarine", "calories": 60},
+        {"name": "orange", "calories": 80},
+        {"name": "peach", "calories": 60},
+        {"name": "pear", "calories": 100},
+        {"name": "pineapple", "calories": 50},
+        {"name": "plums", "calories": 70},
+        {"name": "strawberries", "calories": 50},
+        {"name": "sweet cheries", "calories": 100},
+        {"name": "tangerine", "calories": 50},
+        {"name": "watermelon", "calories": 80},
     ]
 
-    # print result
     calories = ""
+    calories = fruits.name[{item}]
+
+
+    # print result
     print(calories)
 
 

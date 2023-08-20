3d8203be7c90d7e3c4fc6d12b23ebe468f848c5e
Xander136 <113091448+Xander136@users.noreply.github.com>
2023-08-20 03:57:33 +0000

    Sun, Aug 20, 2023, 11:57 AM +08:00

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -2,10 +2,13 @@
 words = input("Input: ")
 
 
-vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
+# vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
+vowels = ["a", "e", "i", "o", "u"]
 
 for vowel in vowels:
-    # remove lower case
+    # remove lower case vowels
     words = words.replace(vowel, "")
+    # remove upper case vowels
+    words = words.replace(vowel.upper(), "")
 
 print(words)
